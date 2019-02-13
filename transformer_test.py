bab77d924be2b81670ab4cbf427ada73d6c33b1f
Suyash <suyash93@protonmail.com>
2019-02-13 10:49:39 +0530

    add tests for subsequent mask and positional encoding

--- transformer_test.py
+++ transformer_test.py
@@ -47,6 +47,59 @@ class TransformerTest(tf.test.TestCase):
             ])
             np.testing.assert_array_equal(mask, expected)
 
+    def test_create_subsequent_mask(self):
+        """
+        https://github.com/lilianweng/transformer-tensorflow/blob/master/transformer_test.py#L68
+        """
+        with self.test_session() as sess:
+            data = np.zeros((self.batch_size, self.seq_len))
+            mask = sess.run(
+                self.t.create_subsequent_mask(self.raw_input),
+                feed_dict={self.raw_input: data})
+            expected = [np.tril(np.ones(
+                (self.seq_len, self.seq_len)))] * self.batch_size
+            np.testing.assert_array_equal(mask, expected)
+
+    def test_positional_encoding_sinusoid(self):
+        """
+        https://github.com/lilianweng/transformer-tensorflow/blob/master/transformer_test.py#L96
+        """
+        with self.test_session() as sess:
+            self.t.d_model = 8
+            encoding = sess.run(
+                self.t.positional_encoding(self.raw_input),
+                feed_dict={self.raw_input: self.fake_data})
+            assert encoding.shape == (4, 5, 8)
+
+            np.testing.assert_array_equal(encoding[0], encoding[1])
+            np.testing.assert_array_equal(encoding[0], encoding[2])
+            np.testing.assert_array_equal(encoding[0], encoding[3])
+
+            # single position
+            np.testing.assert_array_equal(
+                encoding[0][0],
+                np.array([
+                    np.sin(0),
+                    np.cos(0),
+                    np.sin(0),
+                    np.cos(0),
+                    np.sin(0),
+                    np.cos(0),
+                    np.sin(0),
+                    np.cos(0)
+                ]))
+
+            # multiple positions in a single dimension
+            np.testing.assert_array_equal(
+                encoding[0][:, 2],
+                np.array([
+                    np.sin(0),
+                    np.sin(1 / np.power(10000.0, 2.0 / 8.0)),
+                    np.sin(2 / np.power(10000.0, 2.0 / 8.0)),
+                    np.sin(3 / np.power(10000.0, 2.0 / 8.0)),
+                    np.sin(4 / np.power(10000.0, 2.0 / 8.0)),
+                ]).astype(np.float32))
+
 
 if __name__ == '__main__':
     tf.test.main()
